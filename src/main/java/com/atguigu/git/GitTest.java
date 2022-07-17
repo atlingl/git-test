0e5b464d9117e5c027dbc65e7f40803d61d92888
atlingl <109408692+atlingl@users.noreply.github.com>
2022-07-17 14:48:25 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test1!");
         System.out.println("master test2!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
